d69953be9fd6359e41f32edf2786a7b114da7c89
repom2 <repom2@riseup.net>
2021-09-07 16:42:55 +0300

    use docker-compose for build

--- docker-save.sh
+++ docker-save.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build . || exit $?
+docker-compose build || exit $?
 docker save nhparser-bot | zstd - -o "nhparser-$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)-docker.tar.zst"
 
