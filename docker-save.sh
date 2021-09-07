59c642259ca284b1aa20445f72aeb01476a38d6d
repom2 <repom2@riseup.net>
2021-09-07 15:01:05 +0300

    build in docker-save helper

--- docker-save.sh
+++ docker-save.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
+docker build . || exit $?
 docker save nhparser-bot | zstd - -o "nhparser-$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)-docker.tar.zst"
 
